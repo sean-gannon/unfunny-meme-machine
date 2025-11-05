32d4874d91cb2a55c96cfcd93b8bc015dc23f9c5
sean <s.gannon17@universityofgalway.com>
2025-11-05 15:22:21 +0000

    initailize back aenb dw

---
+++ backend/memeMachine/src/test/java/com/fyp/memeMachine/MemeMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.memeMachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
